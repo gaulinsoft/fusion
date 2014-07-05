cbfe75e2e615e0236bf2aefe1b9b4152fdd476d7
Nick Gaulin <ndgaulin@gmail.com>
2014-07-05 03:47:04 -0500

    add core

---
+++ src/core/main.js
@@ -0,0 +1,27 @@
+﻿/*! ------------------------------------------------------------------------
+//                                   Fusion
+//  ------------------------------------------------------------------------
+//
+//                       Copyright 2014 Nicholas Gaulin
+//
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//                   You may obtain a copy of the License at
+//
+//                 http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//                       limitations under the License.
+*/
+
+// ########## MAIN ##########
+
+// Create the global namespace
+var fusion = function(){};
+
+// Define the readonly fields on the global namespace
+$_defineField('dev', !$_minify);
+$_defineField('version', $_version);
