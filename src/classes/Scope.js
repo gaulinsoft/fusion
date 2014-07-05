f54805afaabfcb32fb62df2fe46e21337b94f2ef
Nick Gaulin <ndgaulin@gmail.com>
2014-07-05 03:47:17 -0500

    add classes

---
+++ src/classes/Scope.js
@@ -0,0 +1,61 @@
+﻿/*! ------------------------------------------------------------------------
+//                                   Fusion
+//  ------------------------------------------------------------------------
+//
+//                       Copyright 2014 Nicholas Gaulin
+//
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//                   You may obtain a copy of the License at
+//
+//                 http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//                       limitations under the License.
+*/
+
+// ########## SCOPE ##########
+var Scope   = function($state)
+{
+    // Set the scope parameters
+    this.state = $state;
+},
+  __Scope__ = Scope.prototype = {};
+
+// --- PROTOTYPE ---
+__Scope__.braces      = 0;
+__Scope__.brackets    = 0;
+__Scope__.parentheses = 0;
+__Scope__.state       = '';
+__Scope__.tag         = '';
+__Scope__.tags        = 0;
+
+$_data(__Scope__, 'clone',  function()
+{
+    // Create the scope
+    var $scope = new Scope(this.state);
+
+    // Copy the scope parameters
+    $scope.braces      = this.braces;
+    $scope.brackets    = this.brackets;
+    $scope.parentheses = this.parentheses;
+    $scope.state       = this.state;
+    $scope.tag         = this.tag;
+    $scope.tags        = this.tags;
+
+    // Return the scope
+    return $scope;
+}, true);
+$_data(__Scope__, 'equals', function($scope)
+{
+    // Return true if the scope has the same state and counts
+    return (this.braces      === $scope.braces
+         && this.brackets    === $scope.brackets
+         && this.parentheses === $scope.parentheses
+         && this.state       === $scope.state
+         && this.tag         === $scope.tag
+         && this.tags        === $scope.tags);
+}, true);
---
+++ src/classes/Token.js
@@ -0,0 +1,86 @@
+﻿/*! ------------------------------------------------------------------------
+//                                   Fusion
+//  ------------------------------------------------------------------------
+//
+//                       Copyright 2014 Nicholas Gaulin
+//
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//                   You may obtain a copy of the License at
+//
+//                 http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//                       limitations under the License.
+*/
+
+// ########## TOKEN ##########
+var Token   = function($type, $source, $start, $end)
+{
+    // Set the token parameters
+    this.type   = $type;
+    this.source = $source;
+    this.start  = $start;
+    this.end    = $end;
+},
+  __Token__ = Token.prototype = {};
+
+// --- PROTOTYPE ---
+__Token__.end    = 0;
+__Token__.source = '';
+__Token__.start  = 0;
+__Token__.type   = '';
+
+$_data(__Token__, 'clone',  function()
+{
+    // Return a copy of this token
+    return new Token(this.type,
+                     this.source,
+                     this.start,
+                     this.end);
+}, true);
+$_data(__Token__, 'equals', function($token)
+{
+    // Return true if the token has the same type and text
+    return (this.type   === $token.type
+         && this.text() === $token.text());
+}, true);
+$_data(__Token__, 'html',   function()
+{
+    // Create the HTML text of the token
+    var $text = this.text().replace($_const_entity_search, $_entity);
+
+    // If the token doesn't have a type or it is either whitespace or plain text, return the HTML text
+    if (!this.type || $_lexer_whitespace(this.type) || $_lexer_text(this.type))
+        return $text;
+
+    // Return the HTML of the token
+    return '<span class="' + $_const_class_prefix + this.type + '">' + $text + '</span>';
+}, true);
+$_data(__Token__, 'node',   function()
+{
+    // If the token doesn't have a type or it is either whitespace or plain text, return a text node of the token text
+    if (!this.type || $_lexer_whitespace(this.type) || $_lexer_text(this.type))
+        return document.createTextNode(this.text());
+
+    // Create the element
+    var $element = document.createElement('span');
+
+    // If the token has a type, set the element class
+    if (this.type)
+        $element.setAttribute('class', $_const_class_prefix + this.type);
+
+    // Set the element text
+    $element.innerText = this.text();
+
+    // Return the element
+    return $element;
+}, true);
+$_data(__Token__, 'text',   function()
+{
+    // Return the token text from the source
+    return this.source.substring(this.start, this.end);
+}, true);
