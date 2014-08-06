2d6814b62e187039d0a804d982b38e341bafe605
Nick Gaulin <ndgaulin@gmail.com>
2014-08-06 18:41:06 -0500

    add bundle.cs

---
+++ examples/bundle.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+using Gaulinsoft.Web.Fusion;
+using Gaulinsoft.Web.Optimization;
+
+public class BundleConfig
+{
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+        // Create the fusion bundle and add it to the bundles collection
+        bundles.Add(new FusionBundle("~/js/bundle.js").Include
+        (
+            "~/Scripts/file1.fjs",
+            "~/Scripts/file2.fjs",
+            "~/Scripts/file3.fjs"
+        ));
+    }
+}
