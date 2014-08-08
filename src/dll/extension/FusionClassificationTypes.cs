d472216eeb0cadb9c8e805bb7ecf5c4d1842950c
Nick Gaulin <ndgaulin@gmail.com>
2014-08-08 06:31:08 -0500

    add extension

---
+++ src/dll/extension/FusionClassificationTypes.cs
@@ -0,0 +1,64 @@
+﻿/*! ------------------------------------------------------------------------
+//                                   Fusion
+//  ------------------------------------------------------------------------
+//
+//                       Copyright 2014 Nicholas Gaulin
+//
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//                   You may obtain a copy of the License at
+//
+//                 http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//                       limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension
+{
+    internal static class FusionClassificationTypes
+    {
+        internal const string JavaScriptIdentifier    = "JavaScriptIdentifier";
+        internal const string JavaScriptReservedWord  = "JavaScriptReservedWord";
+        internal const string JavaScriptPunctuator    = "JavaScriptPunctuator";
+        internal const string JavaScriptNumber        = "JavaScriptNumber";
+        internal const string JavaScriptString        = "JavaScriptString";
+        internal const string JavaScriptRegExp        = "JavaScriptRegExp";
+        internal const string JavaScriptComment       = "JavaScriptComment";
+        internal const string JavaScriptText          = "JavaScriptText";
+        internal const string JavaScriptWhitespace    = "JavaScriptWhitespace";
+        internal const string HTMLText                = "HTMLText";
+        internal const string HTMLTagOpenClose        = "HTMLTagOpenClose";
+        internal const string HTMLTagName             = "HTMLTagName";
+        internal const string HTMLTagText             = "HTMLTagText";
+        internal const string HTMLTagWhitespace       = "HTMLTagWhitespace";
+        internal const string HTMLCharacterReference  = "HTMLCharacterReference";
+        internal const string HTMLAttributeName       = "HTMLAttributeName";
+        internal const string HTMLAttributeValue      = "HTMLAttributeValue";
+        internal const string HTMLComment             = "HTMLComment";
+        internal const string HTMLBogusComment        = "HTMLBogusComment";
+        internal const string HTMLDOCTYPE             = "HTMLDOCTYPE";
+        internal const string HTMLCDATA               = "HTMLCDATA";
+        internal const string CSSSelector             = "CSSSelector";
+        internal const string CSSAtRule               = "CSSAtRule";
+        internal const string CSSDeclarationName      = "CSSDeclarationName";
+        internal const string CSSDeclarationValue     = "CSSDeclarationValue";
+        internal const string CSSDeclarationImportant = "CSSDeclarationImportant";
+        internal const string CSSComment              = "CSSComment";
+        internal const string CSSText                 = "CSSText";
+        internal const string CSSWhitespace           = "CSSWhitespace";
+        internal const string FusionTagOpenClose      = "FusionTagOpenClose";
+        internal const string FusionAttributeString   = "FusionAttributeString";
+        internal const string FusionSubstitution      = "FusionSubstitution";
+        internal const string FusionText              = "FusionText";
+        internal const string Unknown                 = "Unknown";
+    }
+}
---
+++ src/dll/extension/FusionHighlighter.cs
@@ -0,0 +1,142 @@
+﻿/*! ------------------------------------------------------------------------
+//                                   Fusion
+//  ------------------------------------------------------------------------
+//
+//                       Copyright 2014 Nicholas Gaulin
+//
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//                   You may obtain a copy of the License at
+//
+//                 http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//                       limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gaulinsoft.VisualStudio.EditorExtensions;
+using Gaulinsoft.Web.Fusion;
+using Microsoft.VisualStudio.Text;
+
+namespace extension
+{
+    internal class FusionHighlighter : IHighlighter<FusionHighlighter, FusionToken>
+    {
+        public FusionHighlighter()
+        {
+            // Create the highlighter
+            this.Highlighter = new Highlighter();
+        }
+
+        internal Highlighter Highlighter = null;
+
+        public string Language
+        {
+            get
+            {
+                // Return the language of the highlighter
+                return this.Highlighter.Lexer.Language;
+            }
+            set
+            {
+                // Set the language of the highlighter
+                this.Highlighter.Lexer.Language = value;
+            }
+        }
+
+        public int Position
+        {
+            get
+            {
+                // Return the position of the highlighter
+                return this.Highlighter.Lexer.Position;
+            }
+            set
+            {
+                // Set the position of the highlighter
+                this.Highlighter.Lexer.Position = value;
+            }
+        }
+
+        public string Source
+        {
+            get
+            {
+                // Return the source code of the highlighter
+                return this.Highlighter.Lexer.Source;
+            }
+            set
+            {
+                // Set the source code of the highlighter
+                this.Highlighter.Lexer.Source = value;
+            }
+        }
+
+        public FusionHighlighter Clone()
+        {
+            // Return a wrapper highlighter with a cloned fusion highlighter
+            return new FusionHighlighter
+            {
+                Highlighter = this.Highlighter.Clone()
+            };
+        }
+
+        public bool Equals(FusionHighlighter highlighter)
+        {
+            // If the highlighter is null, return false
+            if (highlighter == null)
+                return false;
+
+            return this.Highlighter.Equals(highlighter.Highlighter);
+        }
+
+        private Token _token = null;
+
+        public FusionToken Next()
+        {
+            // Get the next fusion token from the highlighter
+            var token = this._token ?? this.Highlighter.Next();
+
+            // If there's no fusion token, return null
+            if (token == null)
+                return null;
+
+            // If the token opens a DOCTYPE
+            if (this._token == null && token.Type == Token.HTMLDOCTYPEOpen)
+            {
+                // Create two copies of the token
+                this._token = token.Clone();
+                token       = token.Clone();
+
+                // Trim the first character from the cached token (which will be returned on the next invocation)
+                this._token.Start++;
+
+                // Trim all but the first character from the copied token (so it matches the closing classification format)
+                token.End  = token.Start + 1;
+                token.Type = Token.HTMLDOCTYPEClose;
+            }
+            // If there's a cached token, remove it
+            else if (this._token != null)
+                this._token = null;
+
+            // Return a wrapper token of the fusion token
+            return new FusionToken
+            {
+                Token = token
+            };
+        }
+
+        public ITrackingPoint TrackingPoint
+        {
+            get;
+            set;
+        }
+    }
+}
